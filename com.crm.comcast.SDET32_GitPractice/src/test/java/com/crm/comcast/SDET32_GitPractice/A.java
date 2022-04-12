e1932d80ab53725e975e2223f8aa8cc14c622fea
coolv <coolv@Vipin>
2022-04-12 11:33:31 +0530

    Commit 1

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/A.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class A 
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("First Push");
+	}
+}
